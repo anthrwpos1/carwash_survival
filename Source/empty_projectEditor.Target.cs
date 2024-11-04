705ada0178dbf50b55cb2dea8e164c0f0a5c9420
Автомойка самовыживания <naprimer@andrey.com>
2024-11-04 09:13:06 +0300

    change ue version

--- Source/empty_projectEditor.Target.cs
+++ Source/empty_projectEditor.Target.cs
@@ -8,7 +8,7 @@ public class empty_projectEditorTarget : TargetRules
 	public empty_projectEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		ExtraModuleNames.AddRange( new string[] { "empty_project" } );
 	}
 }
